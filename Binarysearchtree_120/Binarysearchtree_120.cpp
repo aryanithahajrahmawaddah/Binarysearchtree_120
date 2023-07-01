a7f41fddde8516404fd7ba5cb6eafcc18f632bf4
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-07-02 00:13:53 +1200

    jika data yang dicari lebih kecil

--- Binarysearchtree_120/Binarysearchtree_120.cpp
+++ Binarysearchtree_120/Binarysearchtree_120.cpp
@@ -58,6 +58,16 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{ 
-			// this function searches the currentNode of the specifed Node as well as the current Node of its parent
+		// this function searches the currentNode of the specifed Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			
+		}
 	}
+
 };
