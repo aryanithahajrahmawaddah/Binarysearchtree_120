5ae0204bf253f0641d9d5723166ff25a83a9a075
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-07-01 23:09:34 +1200

    Add project files.

---
+++ Binarysearchtree_120/Binarysearchtree_120.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	// constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
+
+class BinaryTree
+{
+public:
+	Node* ROOT;
+
+	BinaryTree()
+	{
+		ROOT = NULL;			// initializing ROOT TO null		
+	}
+
+	void insert(string element)		// insert a node in the binary search tree
+	{
+
+	}
+};
